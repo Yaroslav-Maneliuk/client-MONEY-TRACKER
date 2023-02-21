64fe0bd994cd834512c99de708e18b78af2a188c
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2023-02-21 20:19:18 +0200

    linked to the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./reset.css"
 import "./App.css";
 
@@ -7,14 +7,60 @@ function App() {
   const [name, setName] = useState("")
   const [datetime, setDatetime] = useState("")
   const [description, setDescription] = useState("")
+  const [transactions, setTransactions] = useState([])
 
-  const addNewTransaction = () => {
+  useEffect(() => {
+    getTransactions().then(setTransactions)
+  }, [])
+
+  const getTransactions = async () => {
+    const url = process.env.REACT_APP_API_URL + "/transactions"
+    const response = await fetch(url)
+    return await response.json()
 
   }
 
+  const addNewTransaction = (e) => {
+    e.preventDefault()
+    const url = process.env.REACT_APP_API_URL + "/transaction"
+    const price = name.split(" ")[0]
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        price,
+        name: name.substring(price.length + 1),
+        datetime,
+        description
+      })
+    })
+      .then(response => {
+        response.json()
+          .then(json => {
+            setName("")
+            setDatetime("")
+            setDescription("")
+            console.log("result", json)
+          })
+      })
+
+  }
+
+  let balance = 0
+  for (const transaction of transactions) {
+    balance = balance + transaction.price
+  }
+
+  balance = balance.toFixed(2)
+  const fraction = balance.split(".")[1]
+  balance = balance.split(".")[0]
+
   return (
     <main >
-      <h1>$400<span>.00</span></h1>
+      <h1>${balance}<span>{fraction}</span></h1>
       <form onSubmit={addNewTransaction}>
         <div className="basic_information">
           <input type="text"
@@ -35,38 +81,21 @@ function App() {
         <button type="submit">Add new transaction</button>
       </form>
       <div className="transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Samsung TV</div>
-            <div className="description">it was time for new TV</div>
+        {transactions.length > 0 && transactions.map(transaction => (
+          <div className="transaction" key={transaction._id}>
+            <div className="left">
+              <div className="name">{transaction.name}</div>
+              <div className="description">{transaction.description}</div>
+            </div>
+            <div className="right">
+              <div className={"price " + (transaction.price < 0 ? "red" : "green")}>
+                {transaction.price}
+              </div>
+              <div className="datetime">{transaction.datetime}</div>
+            </div>
           </div>
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
+        ))}
 
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Gig job new website</div>
-            <div className="description">it was time for new TV</div>
-          </div>
-          <div className="right">
-            <div className="price green">+$400</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
-
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Iphone</div>
-            <div className="description">it was time for new TV</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$900</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
       </div>
     </main>);
 }
